97c912566a35680cc37efbce1ea1707eb6479ed1
Marcin Nowak <marcin.nowak@sonyx.net>
2017-10-04 19:41:18 +0200

    mysql based counting actually works now

--- mqtt-processor/mqtt-processor.js
+++ mqtt-processor/mqtt-processor.js
@@ -1,6 +1,16 @@
 var activeWateringCircuit = 0;
+
 var mqtt = require('mqtt')
+var mysql = require('mysql');
 var client  = mqtt.connect('mqtt://10.10.1.3')
+var connection = mysql.createConnection({
+    host: '10.10.1.3',
+    user: 'mqtt-processor',
+    password: 'tofatofa',	
+    insecureAuth: true,
+    database: 'watering'
+});
+connection.connect();  	
  
 client.on('connect', function () {
   client.subscribe('presence')
@@ -14,7 +24,23 @@ client.on('message', function (topic, message) {
   // message is Buffer
   switch( topic ){
   	case "flow":
-		console.log(activeWateringCircuit + " : " + message.toString());
+   		var newAmount = parseInt(message)
+
+		//console.log(activeWateringCircuit + " : " + message.toString());
+	    connection.query('SELECT * FROM `watering` WHERE DATE(`time`) = CURDATE() AND circuit = ' + activeWateringCircuit  , function(err, rows, fields) {
+	    	
+	    	// if we have an entry....
+	    	if( rows.length > 0 ){
+	    		//console.log(rows[0].amount)
+	    		var currentValue = rows[0].amount;
+	    		console.log( "Current value: " + currentValue + " amount: " + newAmount);
+	    		var newValue = currentValue + newAmount;
+			    connection.query('UPDATE `watering` SET amount = ' + newValue + ' WHERE DATE(`time`) = CURDATE() AND circuit = ' + activeWateringCircuit  , function(err, rows, fields) {});
+	    	}
+	    	else {
+			    connection.query('INSERT INTO `watering` (`time`,`circuit`,`amount`,`duration`) VALUES( CURDATE(), ' + activeWateringCircuit + ', ' + newAmount + ', -1 )'  , function(err, rows, fields) {});
+	    	}
+	    });
 		break;
 	case "/watering":
 		if( message.toString() != "0") {
